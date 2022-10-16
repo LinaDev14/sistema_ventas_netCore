1d729f28aa55074de20eb4c35195c638a6387267
Linadev14 <lina.guerrero@inchcape.com>
2022-10-16 13:22:51 -0500

    Se crea estructura de carpetas

---
+++ SistemaVenta.IOC/Dependencia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVenta.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+//using SistemaVentas.BLL.Implementacion;
+//using SistemaVentas.BLL.Interfaces;
+//using SistemaVenta.DAL.Implementacion;
+//using SistemaVenta.DAL.Interfaces;
+
+namespace SistemaVenta.IOC
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencia(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DBVENTAContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString(
+                    "Server=DDC-CO-LGUERRE; DataBase=DBVENTA; Integrated Security=true; Trusted_Connection=True"));
+            });
+        }
+    }
+}
